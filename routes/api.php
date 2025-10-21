9e7379bff5949be4b3bc3a51c7eb9de045d88278
Argi2912 <argimirojrodriguezrr@gmail.com>
2025-10-21 14:29:09 -0400

    add permissions and role

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,21 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PermissionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'permissions'
+], function () {
+    Route::get('/', [PermissionController::class, 'index']);
+    Route::post('/', [PermissionController::class, 'createPermission']);
+    Route::post('/role', [PermissionController::class, 'createRole']);
+    Route::post('/role-permissions', [PermissionController::class, 'assignRolePermissions']);
+    Route::post('/user-roles', [PermissionController::class, 'assignUserRoles']);
+    Route::post('/user-permissions', [PermissionController::class, 'assignUserDirectPermissions']);
+});
 
 Route::group([
     'middleware' => 'api',
